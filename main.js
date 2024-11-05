271c5b7fe6f16bfd58f7e9916b5ecd7e46dc3d43
Himkator <himkator@gmail.com>
2024-11-05 23:25:29 +0500

    Recipes from massive

---
+++ main.js
@@ -0,0 +1,64 @@
+//import recipes
+import { recipes } from '/recipe.js'
+
+
+//get part where all recipes
+const div_recipe=document.getElementById('recipes')
+
+
+//divide them by 3(the easiest way)
+//TODO improve this part for more situation(for example if length cant be divided by 3)
+for(let i=0; i<recipes.length; i+=3){
+    //create group for 3 reciepes
+    const groupDiv = document.createElement('div');
+    groupDiv.classList.add(`groupRecipes${i}`, 'col', '12', 'recipes-cards-part')
+    for(let j=i; j<i+3 && j<recipes.length; j++){
+        //take recipe by id
+        const recipe = recipes[j];
+
+        //create div card for filling with data
+        const recipeDiv = document.createElement('div');
+        recipeDiv.classList.add('card', 'recipe-card');
+
+        //add image
+        const img = document.createElement('img');
+        img.src = 'grey.jpg';
+        img.alt = recipe.title;
+        img.classList.add('card-img-top')
+        recipeDiv.appendChild(img);
+        
+        //add div for card body
+        const recipeBody = document.createElement('div');
+        recipeBody.classList.add('card-body');
+        recipeDiv.appendChild(recipeBody)
+
+        //fill card body
+        const title = document.createElement('h2');
+        title.textContent = recipe.title;
+        title.classList.add('card-title')
+        recipeBody.appendChild(title);
+
+        const description = document.createElement('p');
+        description.textContent = recipe.description;
+        description.classList.add('card-text')
+        recipeBody.appendChild(description);
+
+        //add button
+        const linkToRecipe=document.createElement('a');
+        //add eventlistener that will be use when click on button
+        linkToRecipe.addEventListener('click', ()=>toRecipePage(j));
+        linkToRecipe.text="Let's cook"
+        linkToRecipe.classList.add('btn', 'btn-primary')
+        recipeBody.appendChild(linkToRecipe)
+
+        groupDiv.appendChild(recipeDiv);
+    }
+    //append child
+    div_recipe.appendChild(groupDiv)
+}
+//for click button
+//TODO Improve this function
+function toRecipePage(indexOfRecipe){
+    console.log(indexOfRecipe)
+}
+
